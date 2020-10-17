d069fbd9cb38727515cdebba3634c37618cd51e8
vvittis <vvittis@isc.tuc.gr>
2020-10-17 18:15:29 +0300

    Vigenere Completed

--- simple_producer.c
+++ simple_producer.c
@@ -89,30 +89,30 @@ char *ceasar_cipher_encryption(char *plainText, int key) {
 
 }
 
-char *ceasar_cipher_decryption(char *plainText, int key) {
+char *ceasar_cipher_decryption(char *crypto, int key) {
     //Decryption
     static char *cipher;
-    cipher = malloc(strlen(plainText) * sizeof(char));
+    cipher = malloc(strlen(crypto) * sizeof(char));
     printf("[Ceasars] decrypted: ");
-    for (int i = 0; i < strlen(plainText); i++) {
-        if (isalpha(plainText[i]) != 0) {
-            if (islower(plainText[i]) != 0) {
+    for (int i = 0; i < strlen(crypto); i++) {
+        if (isalpha(crypto[i]) != 0) {
+            if (islower(crypto[i]) != 0) {
                 int lower_alpha_pos;
                 // printf("Inside Lower\n");
                 // printf("\n2 %d\n",( (int)plainText[i]- 48 ) );
-                lower_alpha_pos = (int) plainText[i] - 61;
+                lower_alpha_pos = (int) crypto[i] - 61;
                 cipher[i] = (char) AlphabetWithDigits[(lower_alpha_pos - key) % 62][0];
-            } else if (isupper(plainText[i]) != 0) {
+            } else if (isupper(crypto[i]) != 0) {
                 int upper_alpha_pos;
                 // printf("ALpha inside %c\n",AlphabetWithDigits[10][0] );
-                upper_alpha_pos = (int) plainText[i] - 55;
+                upper_alpha_pos = (int) crypto[i] - 55;
                 cipher[i] = (char) AlphabetWithDigits[(upper_alpha_pos - key) % 62][0];
             }
             // printf("\n1 %d\n",( (int)plainText[i] ));
-        } else if (isdigit(plainText[i]) != 0) {
+        } else if (isdigit(crypto[i]) != 0) {
             int digit_pos;
             // printf("\n2 %d\n",( (int)plainText[i]- 48 ) );
-            digit_pos = (int) plainText[i] - 48;
+            digit_pos = (int) crypto[i] - 48;
             // printf("HAH %d\n",( digit_pos + key) % 62 );
 
             // printf("ALpha inside %c\n",AlphabetWithDigits[7][0] );
@@ -120,6 +120,7 @@ char *ceasar_cipher_decryption(char *plainText, int key) {
         }
         printf("%c", cipher[i]);
     }
+    scanf("%*c");
     return cipher;
 }
 
@@ -150,28 +151,15 @@ void generateKey(char *str, char *key) {
 }
 
 
-
-
-void Vigenere_Cipher() {
-
-    char key[101];//Assign the key
-    char plainText[101]; //Declare a string for a users text to encrypt
-
-    //Ask the user for a sentence or word to encrypt
-    printf("[Vigenere]: input: ");
-
-    scanf("%s", plainText);
-    printf("[Vigenere] key: ");
-
-    scanf("%s", key);
-
-    generateKey(plainText, key);
-    printf("[Vigenere] key1: %s\n", key);
-    //print the encrypted plain text
-    printf("[Vigenere] encrypted: ");
+char * vigenere_cipher_encryption(char *plainText, char *key){
 
     int i;
-    char cipher[strlen(plainText)];
+    generateKey(plainText, key);
+//    printf("[Vigenere] key1: %s\n", key);
+    //print the encrypted plain text
+    printf("[Vigenere] encrypted: ");
+    static char *cipher;
+    cipher = malloc(strlen(plainText) * sizeof(char));
     int cipherValue;
     int len = strlen(key);
 
@@ -184,30 +172,38 @@ void Vigenere_Cipher() {
         printf("%c",cipher[i]);
 
     }
+    cipher[i] = '\0';
     printf("\n");
+    return cipher;
+}
+
+
+char * vigenere_cipher_decryption(char *crypto, char *key){
+    int len = strlen(key);
+    int i;
     printf("[Vigenere] decrypted: ");
     char de;
     int decipherValue;
-    cipher[i] = '\0';
+    crypto[i] = '\0';
     //Loop through the length of the plain text string
-    for (i = 0; i < strlen(cipher); i++) {
+    for (i = 0; i < strlen(crypto); i++) {
         //if the character is lowercase, where range is [97 -122]
-        if (islower(cipher[i])) {
-            decipherValue = (((int) cipher[i] - 97) - ((int) tolower(key[i % len]) - 97) + 26) % 26 + 97;
+        if (islower(crypto[i])) {
+            decipherValue = (((int) crypto[i] - 97) - ((int) tolower(key[i % len]) - 97) + 26) % 26 + 97;
             de = (char) decipherValue;
 
         } else // Else it's upper case, where letter range is [65 - 90]
         {
-            decipherValue = (((int) cipher[i] - 65) - ((int) toupper(key[i % len]) - 65) + 26) % 26 + 65;
+            decipherValue = (((int) crypto[i] - 65) - ((int) toupper(key[i % len]) - 65) + 26) % 26 + 65;
             de = (char) decipherValue;
         }
 
         //Print the ciphered character if it is alphanumeric (a letter)
-        if (isalpha(cipher[i])) {
+        if (isalpha(crypto[i])) {
             printf("%c", de);
         } else //if the character is not a letter then print the character (e.g. space)
         {
-            printf("%c", cipher[i]);
+            printf("%c", crypto[i]);
         }
     }
 
